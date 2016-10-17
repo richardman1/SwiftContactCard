f05c67c9bcebc116eb72fcc0da9bae0ea7f8bdb6
Richard de Jongh <richarddejongh1995@gmail.com>
2016-10-17 13:22:00 +0200

    Person klasse

---
+++ sep1/Person.swift
@@ -0,0 +1,22 @@
+//
+//  Person.swift
+//  Track-2-TableViewController
+//
+//  Created by Diederich Kroeske on 9/3/15.
+//  Copyright (c) 2015 Diederich Kroeske. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Person {
+    var firstName : String?
+    var lastName: String?
+    var image: UIImage?
+    var email : String?
+    
+    required init()
+    {
+       
+    }
+}
